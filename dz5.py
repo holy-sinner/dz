bf35a753905bf7dd614174bcd14505301104297f
holy-sinner <holy-sinner@inbox.ru>
2023-09-21 22:31:48 +0300

    add dz5

--- dz5.py
+++ dz5.py
@@ -1,13 +1,43 @@
-first = int(input("Введите первый элемент: "))
-step = int(input("Введите шаг: "))
-len = int(input("Введите длину: "))
+# Задача 30:  Заполните массив элементами арифметической прогрессии. Её первый элемент, разность и количество элементов нужно ввести с клавиатуры. Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
+# Каждое число вводится с новой строки.
 
-def res(x,y,z):
-    result = []
-    for i in range(0,z):
-        result.append(x)
-        x = x + y
-    return result
+# first = int(input("Введите первый элемент: "))
+# step = int(input("Введите шаг: "))
+# len = int(input("Введите длину: "))
 
-print(res(first,step,len))
-print(first)
+# def res(x,y,z):
+#     result = []
+#     for i in range(0,z):
+#         result.append(x)
+#         x = x + y
+#     return result
+
+# print(res(first,step,len))
+
+
+# Задача 32: Определить индексы элементов массива (списка),
+# значения которых принадлежат заданному диапазону (т.е. не меньше заданного минимума и не больше заданного максимума)
+
+# data = [int(i) for i in input("Введите числа:").split() ]
+
+# start = int(input("Введите начало диапазона: "))
+# end = int(input("Введите конец диапазона диапазона: "))
+
+# diap = []
+# result = []
+# for i in range(start,end+1):
+#     diap.append(i)
+
+# for i in range(0,len(data)):
+#     if data[i] in diap:
+#         result.append(i)
+
+
+# print(result)
+
+
+
+
+
+
+
